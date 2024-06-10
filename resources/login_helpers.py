c86a2909138b0695ccd4ba6f3428bf57a3507d4b
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-06-10 14:31:00 -0300

    exemplo pipeline Saucedemo Atualizada

---
+++ resources/login_helpers.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options  # Importa as opções do Chrome
+from selenium.webdriver.common.by import By
+import time
+
+def login(username, password):
+    options = Options()  # Cria uma instância de opções
+    options.add_argument("--headless")  # Adiciona a opção headless
+
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+    driver.get("https://www.saucedemo.com/v1/index.html")
+
+    username_field = driver.find_element(By.XPATH, "//*[@id='user-name']")
+    password_field = driver.find_element(By.XPATH, "//*[@id='password']")
+
+    username_field.send_keys(username)
+    password_field.send_keys(password)
+    btnLogin = driver.find_element(By.XPATH, "//*[@id='login-button']")
+    btnLogin.click()
+    time.sleep(5)
+    app_logo = driver.find_element(By.XPATH, "//*[@class='app_logo']")
+    
+    # Verifica se o elemento está visível
+    assert app_logo.is_displayed(), "Componente.app_logo não está visível na página."
+
+    return driver
---
+++ tests/0-login/test_login.py
@@ -0,0 +1,31 @@
+import pytest
+import time
+from resources.autenticacao import *
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from resources.login_helpers import login
+
+   
+def test_login():
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+    driver.get("https://www.saucedemo.com/v1/index.html")
+    print('Pagina acessada com Sucesso!')
+    driver.get_screenshot_as_file("tests/0-login/report/login1.png")
+    username_field = driver.find_element(By.XPATH, "//*[@id='user-name']")  # Use By.ID aqui
+    password_field = driver.find_element(By.XPATH, "//*[@id='password']")
+    username_field.send_keys(username)
+    print('Usuário incluido no campo com Sucesso!')
+    password_field.send_keys(password)
+    print('Password incluido no campo com Sucesso!')
+    driver.get_screenshot_as_file("tests/0-login/report/login2.png")
+    btnLogin = driver.find_element(By.XPATH, "//*[@id='login-button']")
+    btnLogin.click()
+    time.sleep(5)
+    app_logo = driver.find_element(By.XPATH, "//*[@class='app_logo']")
+    assert app_logo.is_displayed(), "Componente.app_logo não está visível na página."  
+    driver.get_screenshot_as_file("tests/0-login/report/login3.png")
+    print('Componente na tela inicial confirmado com Sucesso!')
